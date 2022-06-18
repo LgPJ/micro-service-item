a214cd5e99006dad2dd25a395dc8957072b627ed
Luis <guillermobarreto100@gmail.com>
2022-06-18 19:17:25 -0400

    add eureka server module and microservice registry

--- src/main/java/com/microservice/MicroServiceItemApplication.java
+++ src/main/java/com/microservice/MicroServiceItemApplication.java
@@ -2,10 +2,13 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "product")
+@EnableCircuitBreaker //Notacion para activar Hystrix dentro del proyecto
+@EnableEurekaClient //Notacion para definir el microservicio como cliente eureka
+//@RibbonClient(name = "product") al anexar el EurekaClient se desacopla ribbon, ya que viene dentro de eureka
 @EnableFeignClients
 @SpringBootApplication
 public class MicroServiceItemApplication {
--- src/main/java/com/microservice/controller/ItemController.java
+++ src/main/java/com/microservice/controller/ItemController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.entity.Item;
+import com.microservice.entity.Product;
 import com.microservice.service.ItemServiceImpl;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/item")
@@ -26,9 +28,29 @@ public class ItemController {
 		
 	}
 	
+	//Se indica el metodo que puede dar cortocircuito y dentro se define el metodo alternativo
+	//al cual va a recurrir si falla el mismo
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/id/{id}/cantidad/{cantidad}")
 	public Item findById(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
+	}
+	
+	//El metodo alternativo debe ir de la misma forma del principal, mismos atributos
+	//puede ser o no la misma logica de negocios o una logica alternativa
+	public Item metodoAlternativo(@PathVariable Long id, @PathVariable Integer cantidad) {
+		
+		Item item = new Item();
+		Product product = new Product();
+		
+		item.setCantidad(cantidad);
+		product.setId(id);
+		product.setName("JAJAJAJJA");
+		product.setQty(50.0);
+		item.setProduct(product);
+		return item;
+		
+		//return itemService.findById(id, cantidad);
 	}
 	
 
